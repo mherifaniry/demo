690d536226e8f7d59d7ac732d4099b63cafbed2d
mherifaniry <mherifaniry@gmail.com>
2024-02-18 15:11:53 +0300

    I have created a new IHM called principale.fxml to set the Main IHM more dynamic

--- src/main/java/com/mherifaniry/view/HelloApplication.java
+++ src/main/java/com/mherifaniry/view/HelloApplication.java
@@ -3,9 +3,12 @@ package com.mherifaniry.view;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.stage.WindowEvent;
@@ -18,10 +21,10 @@ public class HelloApplication extends Application {
     double x, y;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(PrincipalesView.class.getResource("principale.fxml"));
+        FXMLLoader fxmlLoader =  PrincipalesView.getContruct();
         Parent root =  fxmlLoader.load();
         Scene scene = new Scene(root, 700, 500);
-        //stage.initStyle(StageStyle.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
         root.setOnMousePressed(evt ->{
             x = evt.getSceneX();
             y = evt.getSceneY();
@@ -30,13 +33,24 @@ public class HelloApplication extends Application {
             stage.setX(evt.getScreenX() - x);
             stage.setY(evt.getScreenY() - y);
         });
+
         scene.getStylesheets().add(PrincipalesView.class.getResource("css/Styling.css").toExternalForm());
 
         stage.setOnShowing(new EventHandler<WindowEvent>() {
             @Override
             public void handle(WindowEvent windowEvent) {
-                PrincipalesView principalesView = fxmlLoader.getController();
-                principalesView.initialLoad();
+
+                FXMLLoader rapportsView = new FXMLLoader(RapportsView.class.getResource("rapports.fxml"));
+                BorderPane borderPane = (BorderPane) root.lookup("#middleBP");
+                try {
+                    borderPane.setCenter(rapportsView.load());
+                    RapportsView rapportsView1 = rapportsView.getController();
+                    rapportsView1.initialLoad();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+
             }
         });
 
--- src/main/java/com/mherifaniry/view/PrincipalesView.java
+++ src/main/java/com/mherifaniry/view/PrincipalesView.java
@@ -1,66 +1,24 @@
 package com.mherifaniry.view;
 
-import com.mherifaniry.wcontroller.TransactionsController;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import mherifaniry.entity.Transactions;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
 
-import java.sql.Timestamp;
+import java.io.IOException;
 
 public class PrincipalesView {
-    @FXML  private TableView<Transactions> table_transactions;
-    @FXML private TableColumn<Transactions, Long> id;
-    @FXML private TableColumn<Transactions, Timestamp> dateDeTransaction;
-    @FXML private TableColumn<Transactions, String> categorie;
-    @FXML private TableColumn<Transactions, String> personne;
-    @FXML private TableColumn<Transactions, Double> montant;
-    @FXML private TableColumn<Transactions, String> description;
 
-    public void initialLoad(){
-        this.loadTransationOnTable(table_transactions, id, dateDeTransaction, categorie, personne, montant, description);
+
+    @FXML
+    private static BorderPane middleBP;
+    private static FXMLLoader fxmlLoader;
+
+    public static FXMLLoader getContruct(){
+        return fxmlLoader = new FXMLLoader(PrincipalesView.class.getResource("principale.fxml"));
     }
 
-    private void loadTransationOnTable(
-            TableView<Transactions> table_transactions,
-            TableColumn<Transactions, Long> id,
-            TableColumn<Transactions, Timestamp> dateDeTransaction,
-            TableColumn<Transactions, String> categorie,
-            TableColumn<Transactions, String> personne,
-            TableColumn<Transactions, Double> montant,
-            TableColumn<Transactions, String> description
-    ){
-
-        ObservableList<Transactions> list = null;
-        TransactionsController transactionsController = new TransactionsController();
-        list = FXCollections.observableArrayList(transactionsController.list());
-        id.setCellValueFactory(new PropertyValueFactory<Transactions, Long>("id"));
-        dateDeTransaction.setCellValueFactory(new PropertyValueFactory<Transactions, Timestamp>("dateDeTransaction"));
-
-        categorie.setCellValueFactory( cellData -> {
-                    Transactions transactions = cellData.getValue();
-                    if (transactions != null && transactions.getCategorie() != null) {
-                        return new SimpleStringProperty(transactions.getCategorie().getLibelle());
-                    } else {
-                        return new SimpleStringProperty("");
-                    }
-                }
-        );
-
-        personne.setCellValueFactory(cellData -> {
-            Transactions transaction = cellData.getValue();
-            if (transaction != null && transaction.getPersonne() != null) {
-                return new SimpleStringProperty(transaction.getPersonne().getName());
-            } else {
-                return new SimpleStringProperty("");
-            }
-        });
-        montant.setCellValueFactory(new PropertyValueFactory<Transactions, Double>("montant"));
-        description.setCellValueFactory(new PropertyValueFactory<Transactions, String>("description"));
-        table_transactions.setItems(list);
+    public static void setMiddleBP(FXMLLoader fxmlLoader) throws IOException {
+        middleBP.setCenter(fxmlLoader.load());
     }
+
 }
